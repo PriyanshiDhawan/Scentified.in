cf2e41129b25f89af7cc2b5b8c99f123e840ce45
PriyanshiDhawan <43516406+PriyanshiDhawan@users.noreply.github.com>
2025-07-12 17:30:50 +0530

    Add serve for Render deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -10,6 +11,8 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
